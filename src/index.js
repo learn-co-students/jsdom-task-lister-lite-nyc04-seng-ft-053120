6427e5bee3063d2da82aa2cf2c7f5a781a3e9dd9
kelseyjdickson <kelseymedved@gmail.com>
2020-08-04 19:24:50 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+// Stable Elements
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form") //gets form from index.html file 
+  const myToDos = document.getElementById("tasks")
+  console.log(myToDos)
+  newTaskForm.addEventListener("submit", function(evt) {
+    evt.preventDefault()
+    let theFormElement = evt.target
+    let userInput =  evt.target["new-task-description"].value //this is giving us the value from the user input
+    let blankListItem = document.createElement("li")
+    blankListItem.innerText = userInput
+    myToDos.append(blankListItem)
+    
+      let blankButton = document.createElement("button")
+      blankButton.innerText = 'x'
+    
+      blankListItem.append(blankButton)
+      myToDos.append(blankListItem)
+
+      blankButton.addEventListener("click", function(evt) {
+        blankListItem.remove()
+      })
+  })
+  let removeAllButton = document.createElement("button")
+  removeAllButton.innerText = 'DELETE ALL'
+
+  myToDos.append(removeAllButton)
+ 
+
+  removeAllButton.addEventListener("click", function(evt) {
+    myToDos.innerHTML = ''
+  })
+  
 });
