563571d57de6a76e6fa90b8db04be41c727fffbe
peawarrior <jacksonchen171@gmail.com>
2020-08-04 17:58:22 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ const sortByPriorityButton = document.querySelector('#sort');
 
 // create new task with delete button
 
-function createTask(newTaskInput, newTaskPriority) {
+function createTask(newTaskInput, newTaskAttributes) {
   let task = document.createElement('li');
   task.innerText = newTaskInput;
-  task.classList.add(newTaskPriority);
+  task.classList.add(newTaskAttributes[priority]);
 
   let deleteButton = document.createElement('button');
-  deleteButton.innerText = 'x'
+  deleteButton.innerText = 'x';
 
+  let editButton = document.createElement('button');
+  editButton.innerText = 'edit';
+
+  task.append(editButton);
   task.append(deleteButton);
   tasks.append(task);
+
+  editButton.addEventListener('click', function(e) {
+    editForm = document.createElement('form');
+    editForm.innerHTML = "<label for='new-task-description'>Task description:</label><br><input type='text' id='new-task-description' name='new-task-description' placeholder='description'><br><label for='priority'>Priority:</label><br><select name='priority' id='priority'><option value='high'>High</option><option value='medium'>Medium</option><option value='low'>Low</option></select><br><input type='submit' value='Edit Task'><br>";
+    task.append(editForm)
+  });
 
   deleteButton.addEventListener('click', function(e) {
     task.remove();
@@ -25,8 +35,11 @@ function createTask(newTaskInput, newTaskPriority) {
 createTaskForm.addEventListener('submit', function(e) {
   e.preventDefault();
   let newTaskInput = (e.target['new-task-description'].value),
-      newTaskPriority = (e.target['priority'].value);
-  createTask(newTaskInput, newTaskPriority);
+      newTaskAttributes = {priority : e.target['priority'].value,
+                           user : e.target['user'].value,
+                           duration : e.target['duration'].value,
+                           duedate : e.target['duedate'].value}
+  createTask(newTaskInput, newTaskAttributes);
 });
 
 // click event to sort
@@ -34,6 +47,8 @@ createTaskForm.addEventListener('submit', function(e) {
 sortByPriorityButton.addEventListener('click', function(e) {
   sortPriority(tasks).forEach(task => {tasks.append(task)})
 });
+
+// custom sort function
 
 function sortPriority(tasks) {
   return Array.from(tasks.children).sort(function(task1, task2) {
