7ddc4f4b5a307df2a0e2b622ee94dea95f7d3f6f
Fraciscv <Francisco.Andaur@icloud.com>
2020-08-08 19:49:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let createNewTask = document.querySelector('#create-task-form')
+  let slapItOnTheDom = document.querySelector('#tasks')
+  
+
+  createNewTask.addEventListener('submit', (evt) =>{
+    evt.preventDefault()
+    
+    createLiElement(evt)
+
+  })
+
+  function createLiElement(evt) {
+    let userInput = (evt.target['new-task-description'].value)
+    let blankLi = document.createElement('li')
+    blankLi.innerText = userInput
+
+    evt.target['new-task-description'].value =""
+    slapItOnTheDom.append(blankLi)
+    blankLi.addEventListener('click', (event) => {
+      blankLi.remove()
+    });
+    
+  }
 });
